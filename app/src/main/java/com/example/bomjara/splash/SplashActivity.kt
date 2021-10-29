346436f7c4c8d200db6ccde17281651393ff51eb
sprada-rus <sappronov@gamil.com>
2021-10-29 17:56:51 +0300

    init commit

---
+++ app/src/main/java/com/example/bomjara/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bomjara.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import com.example.bomjara.R
+import com.example.bomjara.activity.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        progressBar.setProgressDrawableTiled(ContextCompat.getDrawable(this, R.drawable.progresbar))
+
+        var handler = Handler()
+        handler.postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+                finish()
+            },
+            2000
+        )
+    }
+}
